0970281c0583fdc4642e87f672a02c6223d1f823
YuvalFireFly <yuval@gofirefly.io>
2021-11-11 16:04:27 +0200

    Fixing k8s tree bugs

--- collector/k8stree/k8stree.go
+++ collector/k8stree/k8stree.go
@@ -1,32 +1,31 @@
 package k8stree
 
 import (
-	"bytes"
-	"encoding/gob"
-	"encoding/json"
+	"github.com/infralight/k8s-collector/collector/k8s"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"k8s.io/apimachinery/pkg/types"
 )
 
+type ObjectsTree struct {
+	children []*ObjectsTree
+	uid      types.UID
+	kind     string
+	object   unstructured.Unstructured
+}
+
 func GetK8sTree(objects []interface{}) ([]*ObjectsTree, error) {
-	var buf bytes.Buffer
-	enc := gob.NewEncoder(&buf)
-	err := enc.Encode(objects)
-	if err != nil {
-		return nil, err
+	unstructuredObjects := make([]*unstructured.Unstructured, len(objects))
+	for i, obj := range objects {
+		unstructuredObjects[i] = &unstructured.Unstructured{
+			Object: obj.(k8s.KubernetesObject).Object.(map[string]interface{}),
+		}
 	}
 
-	unstructuredObjects := make([]*unstructured.Unstructured, 0, len(objects))
-	err = json.Unmarshal(buf.Bytes(), &unstructuredObjects)
-	if err != nil {
-		return nil, err
-	}
-
-	sourceParents, unstructuredObjects := getSourceParents(unstructuredObjects)
+	sourceParents, remainingUnstructuredObjects := getSourceParents(unstructuredObjects)
 	var objectsTrees []*ObjectsTree
 	var sourceParentTree *ObjectsTree
 	for _, sourceParent := range sourceParents {
-		sourceParentTree, unstructuredObjects = createTrees(sourceParent, unstructuredObjects)
+		sourceParentTree, remainingUnstructuredObjects, _ = createTrees(sourceParent, remainingUnstructuredObjects)
 		objectsTrees = append(objectsTrees, sourceParentTree)
 	}
 	return objectsTrees, nil
@@ -35,50 +34,58 @@ func GetK8sTree(objects []interface{}) ([]*ObjectsTree, error) {
 func getSourceParents(objects []*unstructured.Unstructured) (
 	[]*ObjectsTree, []*unstructured.Unstructured) {
 	sourceParents := make([]*ObjectsTree, 0)
+	remainingChildren := make([]*unstructured.Unstructured, 0)
 
-	for i, obj := range objects {
+	for _, obj := range objects {
 		objOwners := obj.GetOwnerReferences()
 
 		if len(objOwners) == 0 {
 			sourceParents = append(sourceParents, &ObjectsTree{
-				uid: obj.GetUID(),
+				uid:    obj.GetUID(),
+				kind:   obj.GetKind(),
+				object: *obj,
 			})
-			objects = append(objects[:i], objects[i+1:]...)
+			continue
 		}
+		remainingChildren = append(remainingChildren, obj)
 	}
-	return sourceParents, objects
-}
-
-type ObjectsTree struct {
-	children []*ObjectsTree
-	uid      types.UID
+	return sourceParents, remainingChildren
 }
 
 // newObjectDirectory builds object lookup and hierarchy.
 func createTrees(objectsTree *ObjectsTree, objects []*unstructured.Unstructured) (
-	*ObjectsTree, []*unstructured.Unstructured) {
+	*ObjectsTree, []*unstructured.Unstructured, []*unstructured.Unstructured) {
 	foundChildren := make([]*unstructured.Unstructured, 0)
-	missingFatherChildren := objects[:0]
+	remainingChildren := make([]*unstructured.Unstructured, 0)
+	objChildren := make([]*unstructured.Unstructured, 0)
 
 	for _, obj := range objects {
-		if !containsChild(foundChildren, obj) {
+		isChild := false
+		if containsChild(foundChildren, obj) {
 			continue
 		}
 		ownerReference := obj.GetOwnerReferences()
 		for _, ownerRef := range ownerReference {
 			if ownerRef.UID == objectsTree.uid {
 				childObj := &ObjectsTree{
-					uid: obj.GetUID(),
+					uid:    obj.GetUID(),
+					kind:   obj.GetKind(),
+					object: *obj,
 				}
 				objectsTree.children = append(objectsTree.children, childObj)
-				_, objChildren := createTrees(childObj, missingFatherChildren)
+				_, remainingChildren, objChildren = createTrees(childObj, remainingChildren)
 				foundChildren = append(foundChildren, objChildren...)
+				isChild = true
 				break
 			}
 		}
+
+		if !isChild || len(ownerReference) > 1 {
+			remainingChildren = append(remainingChildren, obj)
+		}
 	}
 
-	return objectsTree, foundChildren
+	return objectsTree, remainingChildren, foundChildren
 }
 
 func containsChild(children []*unstructured.Unstructured, child *unstructured.Unstructured) bool {
