8a5911ab4f8d75a13988f2531545a8516e6c0687
YuvalFireFly <yuval@gofirefly.io>
2021-11-15 17:06:41 +0200

    Fix tree bugs

--- collector/k8stree/k8stree.go
+++ collector/k8stree/k8stree.go
@@ -2,6 +2,7 @@ package k8stree
 
 import (
 	"github.com/infralight/k8s-collector/collector/k8s"
+	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 )
 
@@ -23,8 +24,10 @@ func GetK8sTree(objects []interface{}) ([]ObjectsTree, error) {
 	sourceParents, remainingUnstructuredObjects := getSourceParents(unstructuredObjects)
 	var objectsTrees []ObjectsTree
 	var sourceParentTree ObjectsTree
+	var foundChildren []unstructured.Unstructured
 	for _, sourceParent := range sourceParents {
-		sourceParentTree, remainingUnstructuredObjects, _ = createTrees(sourceParent, remainingUnstructuredObjects)
+		sourceParentTree, foundChildren = createTrees(sourceParent, remainingUnstructuredObjects)
+		remainingUnstructuredObjects = differenceUnstructuredObjects(remainingUnstructuredObjects, foundChildren)
 		objectsTrees = append(objectsTrees, sourceParentTree)
 	}
 	return objectsTrees, nil
@@ -51,55 +54,64 @@ func getSourceParents(objects []unstructured.Unstructured) (
 	return sourceParents, remainingChildren
 }
 
-// newObjectDirectory builds object lookup and hierarchy.
 func createTrees(objectsTree ObjectsTree, objects []unstructured.Unstructured) (
-	ObjectsTree, []unstructured.Unstructured, []unstructured.Unstructured) {
+	ObjectsTree, []unstructured.Unstructured) {
 	foundChildren := make([]unstructured.Unstructured, 0)
-	remainingChildren := make([]unstructured.Unstructured, 0)
 	objChildren := make([]unstructured.Unstructured, 0)
 	var childTree ObjectsTree
 
 	for _, obj := range objects {
-		isChild := false
-		if containsChild(foundChildren, obj) {
-			continue
-		}
 		ownerReference := obj.GetOwnerReferences()
-		for i, ownerRef := range ownerReference {
+		for _, ownerRef := range ownerReference {
 			if string(ownerRef.UID) == objectsTree.UID {
-				if i == len(ownerReference)-1 {
-					ownerReference = ownerReference[:i]
-				} else {
-					ownerReference = append(ownerReference[:i], ownerReference[i+1:]...)
-				}
+				ownerReference = differenceOwnerReferences(ownerReference, []v1.OwnerReference{ownerRef})
 				obj.SetOwnerReferences(ownerReference)
+
+				if len(ownerReference) == 0 {
+					foundChildren = append(foundChildren, obj)
+				}
+				remainingChildren := differenceUnstructuredObjects(objects, foundChildren)
 
 				childObj := ObjectsTree{
 					UID:    string(obj.GetUID()),
 					Kind:   obj.GetKind(),
 					Object: obj.Object,
 				}
-				childTree, remainingChildren, objChildren = createTrees(childObj, remainingChildren)
+				childTree, objChildren = createTrees(childObj, remainingChildren)
 				objectsTree.Children = append(objectsTree.Children, childTree)
 				foundChildren = append(foundChildren, objChildren...)
-				isChild = true
 				break
 			}
 		}
-
-		if !isChild || len(ownerReference) != 0 {
-			remainingChildren = append(remainingChildren, obj)
-		}
 	}
 
-	return objectsTree, remainingChildren, foundChildren
+	return objectsTree, foundChildren
 }
 
-func containsChild(children []unstructured.Unstructured, child unstructured.Unstructured) bool {
-	for _, someChild := range children {
-		if someChild.GetUID() == child.GetUID() {
-			return true
+func differenceUnstructuredObjects(a, b []unstructured.Unstructured) []unstructured.Unstructured {
+	mb := make(map[string]struct{}, len(b))
+	for _, x := range b {
+		mb[string(x.GetUID())] = struct{}{}
+	}
+	var diff []unstructured.Unstructured
+	for _, x := range a {
+		if _, found := mb[string(x.GetUID())]; !found {
+			diff = append(diff, x)
 		}
 	}
-	return false
+	return diff
+}
+
+func differenceOwnerReferences(a, b []v1.OwnerReference) []v1.OwnerReference {
+	mb := make(map[string]struct{}, len(b))
+	for _, x := range b {
+		mb[string(x.UID)] = struct{}{}
+	}
+	var diff []v1.OwnerReference
+	for _, x := range a {
+		if _, found := mb[string(x.UID)]; !found {
+			diff = append(diff, x)
+		}
+	}
+	return diff
 }
